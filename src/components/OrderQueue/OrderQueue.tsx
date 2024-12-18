93f375604f0bf3e8939e0aba6f57218ccf1d2da9
Muhammad Irfan <naprirfan@gmail.com>
2024-12-18 16:43:28 +0800

    code cleanup

--- src/components/OrderQueue/OrderQueue.tsx
+++ src/components/OrderQueue/OrderQueue.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
+import { socketKey } from '../../constants';
 
 const SOCKET_SERVER_URL = 'http://localhost:3000';
 const SOCKET_PATH = '/orders';
-const EVENT_NEW_ORDER = 'ORDER_PLACED'; // Replace with the actual event name used by the backend
 
 export const OrderQueue = () => {
     const [orders, setOrders] = useState([]); // Initialize an empty array for orders
     const socket = React.useRef<Socket | null>(null);
 
     useEffect(() => {
-        // Initialize Socket.io connection
         console.log('Connecting to WebSocket server...');
         socket.current = io(SOCKET_SERVER_URL + SOCKET_PATH, {
             path: '/socket.io', // Default Socket.io path
@@ -21,18 +20,15 @@ export const OrderQueue = () => {
             console.log('WebSocket connected:', socket.current?.id);
         });
 
-        // Listen for 'newOrder' events from the server
-        socket.current.on(EVENT_NEW_ORDER, (order) => {
-            console.log('Received new order:', order); // Log the received order
-            setOrders(order); // Append the new order to the list
+        socket.current.on(socketKey.ORDER_PLACED, (order) => {
+            console.log('Received new order:', order);
+            setOrders(order);
         });
 
-        // Handle errors
         socket.current.on('connect_error', (error) => {
             console.error('WebSocket connection error:', error);
         });
 
-        // Cleanup on component unmount
         return () => {
             console.log('Disconnecting WebSocket...');
             socket?.current?.disconnect();
